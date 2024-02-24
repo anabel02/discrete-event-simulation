feef030e8d14fc3f358826fe0a4d40725ecf891a
Raudel Alejandro Gomez Molina <raudel.gmolina@gmail.com>
2024-02-24 16:42:38 -0500

    fix

--- src/simulation/simulator.py
+++ src/simulation/simulator.py
@@ -5,6 +5,7 @@ from typing import List
 
 def simulation(events: List[Event], actionByTime: ActionByTime, state: State, time: float) -> List[Event]:
     history = []
+    last_time = 0
 
     while events:
         event = heappop(events)
@@ -12,9 +13,12 @@ def simulation(events: List[Event], actionByTime: ActionByTime, state: State, ti
         if event.time > time:
             break
 
+        last_time = event.time
         history.append(event)
 
+        actionByTime.action(event.time - last_time, state)
         event.action(state, events)
-        actionByTime.action(event.interval, state)
+
+    actionByTime.action(time-last_time, state)
 
     return history
