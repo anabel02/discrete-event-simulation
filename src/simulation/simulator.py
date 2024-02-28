90661566a4bcc41d2c0dfb5fae0db4577952f37a
Anabel Benítez <anabelbenitezglez@gmail.com>
2024-02-27 21:53:48 -0500

    Add stop case

--- src/simulation/simulator.py
+++ src/simulation/simulator.py
@@ -1,9 +1,18 @@
 from .core import Event, State, ActionByTime
 from heapq import heappop
-from typing import List
+from typing import List, Callable
 
 
-def simulation(events: List[Event], actionByTime: ActionByTime, state: State, time: float) -> List[Event]:
+def simulation(events: List[Event], action_by_time: ActionByTime, state: State, time: float, stop_case: Callable[[State], bool]) -> List[Event]:
+    """
+    Simulate the system. It stops when the stop_case returns True or when the time is over.
+    :param events: List of events
+    :param action_by_time: Change the state simulating the time between events
+    :param state: Initial state of the system
+    :param time: Max time to simulate
+    :param stop_case: Function that returns True if the simulation should stop
+    :return: List of events that happened
+    """
     history = []
     last_time = 0
 
@@ -19,9 +28,9 @@ def simulation(events: List[Event], actionByTime: ActionByTime, state: State, ti
         last_time = event.time
         history.append(event)
 
-        actionByTime.action(event.time - last_time, state)
+        action_by_time.action(event.time - last_time, state)
         event.action(state, events)
 
-    actionByTime.action(time-last_time, state)
+    action_by_time.action(time - last_time, state)
 
     return history
