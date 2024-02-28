1bfbf702ce47608b6d6b42becf69d18b20107878
Anabel Benítez <anabelbenitezglez@gmail.com>
2024-02-27 21:54:34 -0500

    Add doc strings

--- src/simulation/core.py
+++ src/simulation/core.py
@@ -5,8 +5,12 @@ from typing import List
 class State(ABC):
     pass
 
+
 class ActionByTime(ABC):
     def action(self, interval: float, state: State):
+        """
+        This method should be called before each event action. It should be used to update the state of the system.
+        """
         pass
 
 
@@ -25,4 +29,10 @@ class Event(ABC):
         return self.time == other.time
 
     def action(self, state: State, events: List['Event']):
+        """
+        This method should be called when the event is popped from the heap. It should be used to update the state of the system and to generate new events.
+        :param state: The state of the system
+        :param events: The list of events
+        :return: None
+        """
         pass
