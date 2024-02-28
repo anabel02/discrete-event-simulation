23dc674f992e62f439476656587d1dde51a9d7f0
Anabel Benítez <anabelbenitezglez@gmail.com>
2024-02-27 22:28:11 -0500

    Fix some issues. Type of the parameters, check if the parameters are consistent, add stop case.

--- src/inventory/inventory_simulation.py
+++ src/inventory/inventory_simulation.py
@@ -5,39 +5,46 @@ from simulation.core import Event, State
 from simulation.simulator import simulation
 
 
-def generate_events(index: int, time: float, time_dist: Callable[[], None], cant_dist: Callable[[], None], config: InventoryConfig) -> List[Event]:
+def generate_events(index: int, time: float, time_dist: Callable[[], float], cant_dist: Callable[[], int],
+                    config: InventoryConfig) -> List[Event]:
     current = 0
     events = []
 
     def post_action(time: float, state: State, events: List[Event]):
         if state.cants[index] < config.parameter_s:
             heappush(events, RefillEvent(time, config.refill_interval, index, config.parameter_S -
-                     state.cants[index], config.cost_refill(config.parameter_S - state.cants[index])))
+                                         state.cants[index],
+                                         config.cost_refill(config.parameter_S - state.cants[index])))
 
     while current < time:
         interval = time_dist()
         current += interval
         events.append(ShopProductEvent(current, interval, index,
-                      cant_dist(), config.price, post_action))
+                                       cant_dist(), config.price, post_action))
 
     return events
 
 
-def configure(time: float, time_dist: Callable[[], None], configs: List[InventoryConfig], cant_dists: List[Callable[[], None]]) -> List[Event]:
+def configure(time: float, time_dist: Callable[[], float], configs: List[InventoryConfig],
+              cant_dists: List[Callable[[], int]]) -> List[Event]:
     events = []
 
     for i in range(len(configs)):
-        events += generate_events(i, time, time_dist,
-                                  cant_dists[i], configs[i])
+        events += generate_events(i, time, time_dist, cant_dists[i], configs[i])
 
     heapify(events)
 
     return events
 
 
-def inventory_simulation(time: float, time_dist: Callable[[], None], configs: List[InventoryConfig], cant_dists: List[Callable[[], None]], initial_cants: List[int], initial_money: float):
+def inventory_simulation(time: float, time_dist: Callable[[], float], configs: List[InventoryConfig],
+                         cant_dists: List[Callable[[], int]], initial_cants: List[int], initial_money: float,
+                         stop_case: Callable[[InventoryState], bool] = lambda x: False):
+    # Check if the parameters are consistent
+    assert len(configs) == len(cant_dists) == len(initial_cants)
+
     state = InventoryState(initial_cants, initial_money)
     events = configure(time, time_dist, configs, cant_dists)
     action_by_time = ActionByTimeInventory(configs)
 
-    simulation(events, action_by_time, state, time)
+    return simulation(events, action_by_time, state, time, stop_case)
